097ae6aadcc2af2938d3946243eb4dfd6f38a44f
sulfH <39556879+sulfH@users.noreply.github.com>
2019-12-07 17:33:14 +0100

    koordinatensystem

--- Praktikum/app/src/main/java/com/example/praktikum/MainActivity.java
+++ Praktikum/app/src/main/java/com/example/praktikum/MainActivity.java
@@ -3,24 +3,50 @@ package com.example.praktikum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonWeiter;
     Spinner spinnerDatenquellen;
-
+    private GraphView graph;
+    int x = 20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Initialisierung
+
         buttonWeiter = findViewById(R.id.buttonWeiter);
         spinnerDatenquellen = findViewById(R.id.spinnerDatenquellen);
+
+        graph = (GraphView) findViewById(R.id.graph);
+
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMaxX(x);
+        graph.getViewport().setMaxY(100);
+
+        LineGraphSeries<DataPoint>xywerte = new LineGraphSeries<DataPoint>();
+        xywerte.setColor(Color.RED);
+
+
+        int n = 5;
+        for(double i=0;i <= n; i+= 0.1)
+        {
+            DataPoint dp = new DataPoint(i,Math.pow(4,i));
+            xywerte.appendData(dp,false,1000);
+        }
+         graph.addSeries(xywerte);
 
         buttonWeiter.setOnClickListener(new View.OnClickListener() {
             @Override
